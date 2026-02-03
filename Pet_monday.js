ce389c484d0bc5ae78e17a4bb23a243b26579770
kroAton666 <maxjuck00@gmail.com>
2026-02-03 09:36:35 +0200

    fix2.0

--- Pet_monday.js
+++ Pet_monday.js
@@ -2,25 +2,27 @@
     'use strict';
 
     function startPlugin() {
-        // Підписуємося на подію відкриття картки
+        // Чекаємо відкриття картки фільму
         Lampa.Listener.follow('full', 'open', function (data) {
             var object = data.object;
             var component = data.component;
 
-            // Створюємо кнопку з реальною іконкою (Play)
-            var btn = $(
-                '<div class="full-start__button selector view--online" data-subtitle="My Source">' +
-                '<svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"></polygon></svg>' +
+            // Створюємо кнопку "по-старому" (через плюсики), щоб WebOS точно зрозумів
+            var html = '<div class="full-start__button selector view--online" data-subtitle="My Source">' +
+                '<svg width="20" height="20" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">' +
+                '<path d="M8 5V19L19 12L8 5Z" fill="currentColor"/>' +
+                '</svg>' +
                 '<span>Дивитись MySource</span>' +
-                '</div>'
-            );
+                '</div>';
 
-            // Додаємо дію при натисканні (підтримка миші і пульта)
+            var btn = $(html);
+
+            // Дія при кліку
             btn.on('click hover:enter', function () {
                 searchAndPlay(object);
             });
 
-            // Вставляємо кнопку перед іншими або в кінець списку
+            // Додаємо кнопку на екран
             component.find('.full-start__buttons').append(btn);
         });
     }
@@ -28,9 +30,9 @@
     function searchAndPlay(movie) {
         Lampa.Noty.show('Запуск відео: ' + movie.title);
 
-        // Пряме посилання на відео (Ваш приклад)
         var streamUrl = 'https://ashdi.vip/video02/3/serials/blue_lock/sinya_vyazniczya__01_76952/hls/DK6XiHOKjuRemBH9Ag==/index.m3u8';
 
+        // Запуск плеєра
         Lampa.Player.play({
             url: streamUrl,
             title: movie.title,
@@ -41,12 +43,14 @@
             }
         });
 
+        // Додаємо в плейлист
         Lampa.Player.playlist([{
             url: streamUrl,
             title: movie.title
         }]);
     }
 
+    // Запуск плагіна
     if (window.appready) startPlugin();
     else Lampa.Listener.follow('app', 'ready', startPlugin);
 })();
