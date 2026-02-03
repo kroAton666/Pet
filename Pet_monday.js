8f435e9d025716fea320ff15667772e04d850407
kroAton666 <maxjuck00@gmail.com>
2026-02-03 10:23:13 +0200

    update

--- Pet_monday.js
+++ Pet_monday.js
@@ -1,56 +1,286 @@
+// Назва плагіна: UakinoBest
+// Версія: 1.1
+// Опис: Плагін для перегляду з сайту uakino.best
+
 (function () {
     'use strict';
 
+    // Функція для запуску плагіна
     function startPlugin() {
-        // Чекаємо відкриття картки фільму
-        Lampa.Listener.follow('full', 'open', function (data) {
-            var object = data.object;
-            var component = data.component;
+        // Реєструємо компонент в Lampa
+        window.uakino_component = function(object) {
+            var network = new Lampa.Reguest();
+            var scroll = new Lampa.Scroll({ mask: true, over: true });
+            var files = new Lampa.Explorer(object);
 
-            // Створюємо кнопку "по-старому" (через плюсики), щоб WebOS точно зрозумів
-            var html = '<div class="full-start__button selector view--online" data-subtitle="My Source">' +
-                '<svg width="20" height="20" viewBox="0 0 24 24" fill="white" xmlns="http://www.w3.org/2000/svg">' +
-                '<path d="M8 5V19L19 12L8 5Z" fill="currentColor"/>' +
-                '</svg>' +
-                '<span>Дивитись MySource</span>' +
-                '</div>';
+            // Створюємо екземпляр нашого балансера
+            var source = new uakino(this, object);
+            var last;
 
-            var btn = $(html);
+            this.create = function () {
+                this.activity.loader(true);
+                files.appendFiles(scroll.render());
+                this.search(); // Запускаємо пошук
+                return this.render();
+            };
 
-            // Дія при кліку
-            btn.on('click hover:enter', function () {
-                searchAndPlay(object);
-            });
+            this.search = function () {
+                this.activity.loader(true);
+                this.reset();
+                source.search(object.movie.title, object.movie.original_title);
+            };
 
-            // Додаємо кнопку на екран
-            component.find('.full-start__buttons').append(btn);
-        });
-    }
+            // Допоміжні функції для взаємодії з інтерфейсом Lampa
+            this.empty = function(msg) {
+                var empty = Lampa.Template.get('list_empty');
+                if (msg) empty.find('.empty__descr').text(msg);
+                scroll.append(empty);
+                this.loading(false);
+            };
+            this.loading = function(status) {
+                if (status) this.activity.loader(true);
+                else this.activity.loader(false);
+            };
+            this.reset = function() {
+                scroll.clear();
+            };
+            this.append = function(item) {
+                item.on('hover:focus', (e) => {
+                    last = e.target;
+                    scroll.update($(e.target), true);
+                });
+                scroll.append(item);
+            };
+            this.render = function() {
+                return files.render();
+            };
+            this.start = function(first_select) {
+                if (first_select) {
+                    last = scroll.render().find('.selector').eq(0)[0];
+                }
+                Lampa.Controller.add('content', {
+                    toggle: () => {
+                        Lampa.Controller.collectionSet(scroll.render(), files.render());
+                        Lampa.Controller.collectionFocus(last || false, scroll.render());
+                    },
+                    up: () => Lampa.Controller.toggle('head'),
+                    down: () => Navigator.move('down'),
+                    back: this.back
+                });
+                Lampa.Controller.toggle('content');
+            };
+            this.back = function() { Lampa.Activity.backward(); };
+            this.destroy = function () {
+                network.clear();
+                files.destroy();
+                scroll.destroy();
+                source.destroy();
+            };
+        };
 
-    function searchAndPlay(movie) {
-        Lampa.Noty.show('Запуск відео: ' + movie.title);
+        // Логіка балансера для uakino.best
+        function uakino(component, _object) {
+            var network = new Lampa.Reguest();
+            var object = _object;
+            var HOST = 'https://uakino.best';
+            var SEARCH_API = HOST + '/engine/lazydev/dle_search/ajax.php';
+            // Цей хеш може змінитися в майбутньому, тоді його потрібно буде оновити
+            var DLE_HASH = '8018336ddfb7bab82040aba79ce3188e1d05511c';
 
-        var streamUrl = 'https://ashdi.vip/video02/3/serials/blue_lock/sinya_vyazniczya__01_76952/hls/DK6XiHOKjuRemBH9Ag==/index.m3u8';
+            this.destroy = function () {
+                network.clear();
+            };
 
-        // Запуск плеєра
-        Lampa.Player.play({
-            url: streamUrl,
-            title: movie.title,
-            poster: movie.img,
-            timeline: {
-                hash: Lampa.Utils.hash(movie.title),
-                time: 0
+            // Крок 1: Пошук фільму на сайті
+            this.search = function (title, original_title) {
+                var story = encodeURIComponent(title);
+                var post_data = `story=${story}&dle_hash=${DLE_HASH}&thisUrl=/`;
+
+                network.post(SEARCH_API, post_data, (search_html) => {
+                    var parser = new DOMParser();
+                    var doc = parser.parseFromString(search_html, "text/html");
+                    var results = doc.querySelectorAll('a.sres-url');
+
+                    if (results.length > 0) {
+                        // Проста логіка вибору найкращого результату
+                        var best_match = null;
+                        var title_lower = title.toLowerCase();
+                        var original_title_lower = original_title.toLowerCase();
+
+                        results.forEach(link => {
+                            var link_title = link.querySelector('.sres-title').textContent.toLowerCase();
+                            var link_original_title = link.querySelector('.sres-original').textContent.toLowerCase();
+                            if(link_title.includes(title_lower) || link_original_title.includes(original_title_lower)) {
+                                if(!best_match) best_match = link.href;
+                            }
+                        });
+
+                        if(best_match){
+                            getPage(best_match);
+                        } else {
+                            // Якщо точного збігу немає, беремо перший результат
+                            getPage(results[0].href);
+                        }
+                    } else {
+                        component.empty(`За запитом '${title}' нічого не знайдено.`);
+                    }
+                }, (a, c) => {
+                    component.empty('Помилка пошуку. ' + network.errorDecode(a,c));
+                });
+            };
+
+            // Крок 2: Отримання сторінки фільму/серіалу для пошуку плеєра
+            function getPage(movieUrl) {
+                network.get(movieUrl, (page_html) => {
+                    var items = [];
+                    var parser = new DOMParser();
+                    var doc = parser.parseFromString(page_html, "text/html");
+
+                    // Перевіряємо, чи це серіал (чи є вибір серій)
+                    var episode_links = doc.querySelectorAll('.serial-series-box > li > a');
+
+                    if (episode_links.length > 0) { // Це серіал
+                        episode_links.forEach((link, index) => {
+                            items.push({
+                                title: link.textContent.trim(),
+                                episode: index + 1,
+                                iframeSrc: link.getAttribute('onclick').match(/'([^']+)'/)[1] // Витягуємо URL з onclick
+                            });
+                        });
+                    } else { // Це фільм
+                        var iframe = doc.querySelector('iframe#playerfr');
+                        if (iframe) {
+                            items.push({
+                                title: object.movie.title,
+                                episode: 0, // ознака фільму
+                                iframeSrc: iframe.src
+                            });
+                        }
+                    }
+
+                    if (items.length > 0) {
+                        append(items);
+                    } else {
+                        component.empty('Плеєр не знайдено на сторінці.');
+                    }
+                }, (a, c) => {
+                    component.empty('Не вдалося завантажити сторінку фільму.');
+                });
+            }
+
+            // Крок 3: Отримання фінального посилання на потік
+            function getStream(element, callback) {
+                // Якщо посилання вже отримано, повертаємо його
+                if (element.stream) return callback();
+
+                // Додаємо протокол, якщо його немає
+                var iframeUrl = element.iframeSrc.startsWith('//') ? 'https:' + element.iframeSrc : element.iframeSrc;
+
+                network.get(iframeUrl, (player_html) => {
+                    var parser = new DOMParser();
+                    var doc = parser.parseFromString(player_html, "text/html");
+                    var video_tag = doc.querySelector('video');
+
+                    if (video_tag && video_tag.src) {
+                        element.stream = video_tag.src;
+                        callback();
+                    } else {
+                        Lampa.Noty.show('Не вдалося отримати посилання на відеопотік.');
+                        component.loading(false);
+                    }
+                }, (a, c) => {
+                    Lampa.Noty.show('Помилка завантаження плеєра.');
+                    component.loading(false);
+                });
+            }
+
+            // Відображення списку серій/фільму в інтерфейсі Lampa
+            function append(items) {
+                component.reset();
+                items.forEach(element => {
+                    var item = Lampa.Template.get('online_mod', {
+                        title: element.title,
+                        quality: 'HD' // Можна залишити статично, бо якість невідома
+                    });
+
+                    item.on('hover:enter', () => {
+                        element.loading = true;
+                        getStream(element, () => {
+                            element.loading = false;
+
+                            var playlist = [];
+                            var first = {
+                                url: element.stream,
+                                title: element.title
+                            };
+
+                            // Якщо це серіал, створюємо плейлист
+                            if(items.length > 1){
+                                items.forEach(elem => {
+                                    var cell = {
+                                        title: elem.title,
+                                        url: (call) => {
+                                            getStream(elem, () => call(elem.stream));
+                                        }
+                                    };
+                                    if(elem === element) playlist.unshift(cell);
+                                    else playlist.push(cell);
+                                });
+                            } else {
+                                playlist.push(first);
+                            }
+
+                            Lampa.Player.play(first);
+                            Lampa.Player.playlist(playlist);
+                        });
+                    });
+                    component.append(item);
+                });
+                component.loading(false);
+                component.start(true);
+            }
+        }
+
+        // Додаємо кнопку "Онлайн" до карток фільмів
+        Lampa.Listener.follow('full', function (e) {
+            if (e.type == 'complite') {
+                var btn = $(`
+                    <div class="full-start__button selector view--online">
+                        <svg version="1.1" id="Capa_1" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" x="0px" y="0px" viewBox="0 0 477.867 477.867" style="enable-background:new 0 0 477.867 477.867;" xml:space="preserve"><g><path d="M452.267,24.043c-15.893-15.893-41.653-15.893-57.547,0L24.043,394.709c-15.893,15.893-15.893,41.653,0,57.547 c7.947,7.947,18.347,11.92,28.773,11.92s20.827-3.973,28.773-11.92L452.267,81.589C468.16,65.696,468.16,39.936,452.267,24.043z"/><path d="M452.267,24.043c-15.893-15.893-41.653-15.893-57.547,0L24.043,394.709c-15.893,15.893-15.893,41.653,0,57.547 c7.947,7.947,18.347,11.92,28.773,11.92s20.827-3.973,28.773-11.92L452.267,81.589C468.16,65.696,468.16,39.936,452.267,24.043z"/></g></svg>
+                        <span>Uakino</span>
+                    </div>`);
+
+                btn.on('hover:enter', function () {
+                    Lampa.Activity.push({
+                        url: '',
+                        title: 'Uakino',
+                        component: 'uakino_component',
+                        movie: e.data.movie,
+                    });
+                });
+                e.object.activity.render().find('.view--torrent').after(btn);
             }
         });
 
-        // Додаємо в плейлист
-        Lampa.Player.playlist([{
-            url: streamUrl,
-            title: movie.title
-        }]);
+        // Створюємо шаблони, якщо вони не існують
+        if (!Lampa.Template.get('online_mod')) {
+            Lampa.Template.add('online_mod', `
+                <div class="online selector">
+                    <div class="online__body">
+                        <div class="online__title">{title}</div>
+                        <div class="online__quality">{quality}</div>
+                    </div>
+                </div>
+            `);
+        }
     }
 
-    // Запуск плагіна
+    // Запускаємо плагін
     if (window.appready) startPlugin();
-    else Lampa.Listener.follow('app', 'ready', startPlugin);
+    else {
+        Lampa.Listener.follow('app', function (e) {
+            if (e.type == 'ready') startPlugin();
+        });
+    }
+
 })();
