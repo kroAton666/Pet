eb02a89a6cc391be04bf92f50bf790a80beb5aa3
kroAton666 <maxjuck00@gmail.com>
2026-02-03 09:04:54 +0200

    start try

--- Pet_monday.js
+++ Pet_monday.js
@@ -6,7 +6,7 @@
         Lampa.Listener.follow('full', 'open', function (data) {
             // data.object - це об'єкт з інформацією про фільм (назва, id, рік тощо)
             // data.component - посилання на сам компонент картки
-            
+
             let object = data.object;
             let component = data.component;
 
@@ -31,36 +31,32 @@
     function searchAndPlay(movie) {
         // Демонстрація: просто запускаємо тестове відео
         // У реальності тут має бути fetch/ajax запит до вашого джерела
-        
+
         Lampa.Noty.show('Шукаю відео для: ' + movie.title);
 
         // Симуляція затримки мережі
         setTimeout(function(){
-            
+
             // Запуск плеєра
             Lampa.Player.play({
-                url: 'https://ashdi.vip/vod/76952', // Ваше посилання на відео (m3u8/mp4)
-                title: 'Blu lock',// movie.title, // Назва у плеєрі
-                poster: 'https://uakino.best/uploads/mini/serial/d2/9c965394d605d52d4f4896cae3b727.webp'//movie.img,  // Постер (якщо є)
+                url: 'https://ashdi.vip/video02/3/serials/blue_lock/sinya_vyazniczya__01_76952/hls/DK6XiHOKjuRemBH9Ag==/index.m3u8', // Ваше посилання на відео (m3u8/mp4)
+                title: movie.title, // Назва у плеєрі
+                poster: movie.img,  // Постер (якщо є)
                 timeline: {
                     // Якщо потрібно зберегти час перегляду
-                    hash: Lampa.Utils.hash('Blu lock'), //movie.title // унікальний ID
-                    time: 0 
+                    hash: Lampa.Utils.hash(movie.title), // унікальний ID
+                    time: 0
                 }
             });
 
             // Додаємо в плейлист (щоб працювала кнопка "наступна серія" абощо)
             Lampa.Player.playlist([
                 {
-                    url: 'https://ashdi.vip/vod/77157',
-                    title: 'bluLock 2' // movie.title
-                },
-                {
-                    url: 'https://ashdi.vip/vod/77927',
-                    title: 'bluLock 3' // movie.title
+                    url: 'https://ashdi.vip/video02/3/serials/blue_lock/sinya_vyazniczya__01_76952/hls/DK6XiHOKjuRemBH9Ag==/index.m3u8',
+                    title: movie.title
                 }
             ]);
-            
+
         }, 1000);
     }
 
