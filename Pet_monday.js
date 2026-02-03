a624eb0fdf994a5cfb38180129d5a77dc9a47140
kroAton666 <maxjuck00@gmail.com>
2026-02-03 20:54:59 +0200

    fix8

--- Pet_monday.js
+++ Pet_monday.js
@@ -251,6 +251,7 @@
                     </div>`);
 
                 btn.on('hover:enter', function () {
+                    log('push btn')
                     Lampa.Activity.push({
                         url: '',
                         title: 'Uakino',
