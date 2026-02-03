ae402e9eb7f3c6fdc4c0d70b1482eab06d08fcd2
kroAton666 <maxjuck00@gmail.com>
2026-02-03 09:21:01 +0200

    fix

--- Pet_monday.js
+++ Pet_monday.js
@@ -2,62 +2,49 @@
     'use strict';
 
     function startPlugin() {
-        // Підписуємося на подію відкриття картки (full)
+        // Підписуємося на подію відкриття картки
         Lampa.Listener.follow('full', 'open', function (data) {
-            // data.object - це об'єкт з інформацією про фільм (назва, id, рік тощо)
-            // data.component - посилання на сам компонент картки
+            var object = data.object;
+            var component = data.component;
 
-            let object = data.object;
-            let component = data.component;
+            // Створюємо кнопку з реальною іконкою (Play)
+            var btn = $(
+                '<div class="full-start__button selector view--online" data-subtitle="My Source">' +
+                '<svg width="22" height="22" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><polygon points="5 3 19 12 5 21 5 3"></polygon></svg>' +
+                '<span>Дивитись MySource</span>' +
+                '</div>'
+            );
 
-            // Створюємо кнопку
-            let btn = $(`<div class="full-start__button selector view--online" data-subtitle="My Source">
-                <svg>...</svg> <!-- Тут можна вставити SVG іконку -->
-                <span>Дивитись MySource</span>
-                </div>`);
-
-            // Додаємо дію при натисканні
+            // Додаємо дію при натисканні (підтримка миші і пульта)
             btn.on('click hover:enter', function () {
-                // Тут логіка пошуку відео
-                // Наприклад, ми робимо запит до вашого API або парсимо сайт
                 searchAndPlay(object);
             });
 
-            // Вставляємо кнопку в інтерфейс (блок кнопок)
+            // Вставляємо кнопку перед іншими або в кінець списку
             component.find('.full-start__buttons').append(btn);
         });
     }
 
     function searchAndPlay(movie) {
-        // Демонстрація: просто запускаємо тестове відео
-        // У реальності тут має бути fetch/ajax запит до вашого джерела
+        Lampa.Noty.show('Запуск відео: ' + movie.title);
 
-        Lampa.Noty.show('Шукаю відео для: ' + movie.title);
+        // Пряме посилання на відео (Ваш приклад)
+        var streamUrl = 'https://ashdi.vip/video02/3/serials/blue_lock/sinya_vyazniczya__01_76952/hls/DK6XiHOKjuRemBH9Ag==/index.m3u8';
 
-        // Симуляція затримки мережі
-        setTimeout(function(){
+        Lampa.Player.play({
+            url: streamUrl,
+            title: movie.title,
+            poster: movie.img,
+            timeline: {
+                hash: Lampa.Utils.hash(movie.title),
+                time: 0
+            }
+        });
 
-            // Запуск плеєра
-            Lampa.Player.play({
-                url: 'https://ashdi.vip/video02/3/serials/blue_lock/sinya_vyazniczya__01_76952/hls/DK6XiHOKjuRemBH9Ag==/index.m3u8', // Ваше посилання на відео (m3u8/mp4)
-                title: movie.title, // Назва у плеєрі
-                poster: movie.img,  // Постер (якщо є)
-                timeline: {
-                    // Якщо потрібно зберегти час перегляду
-                    hash: Lampa.Utils.hash(movie.title), // унікальний ID
-                    time: 0
-                }
-            });
-
-            // Додаємо в плейлист (щоб працювала кнопка "наступна серія" абощо)
-            Lampa.Player.playlist([
-                {
-                    url: 'https://ashdi.vip/video02/3/serials/blue_lock/sinya_vyazniczya__01_76952/hls/DK6XiHOKjuRemBH9Ag==/index.m3u8',
-                    title: movie.title
-                }
-            ]);
-
-        }, 1000);
+        Lampa.Player.playlist([{
+            url: streamUrl,
+            title: movie.title
+        }]);
     }
 
     if (window.appready) startPlugin();
